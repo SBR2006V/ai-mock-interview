a198956e866ba2a8453dbe5951738b4d516d1df8
SBR2006V <biswassourasish905athlite@gmail.com>
2026-04-22 22:50:08 +0530

    History Button colour change

--- app/page.js
+++ app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
             📄 Resume Analyzer
           </button>
           <button
-            className="roleBtn orange"
+            className="roleBtn red"
             onClick={() => router.push("/history")}
           >
             📊 View History
